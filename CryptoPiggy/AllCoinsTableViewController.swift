2ecbb7f89a7f3cc0493e4c1372526e36f9961101
Paweł Brzozowski <mac@192.168.1.6>
2021-10-22 19:14:06 +0200

    update of validation

--- CryptoPiggy/AllCoinsTableViewController.swift
+++ CryptoPiggy/AllCoinsTableViewController.swift
@@ -54,61 +54,16 @@ class AllCoinsTableViewController: UITableViewController {
         
     }
     
-    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
-
+// Validating if coins from API have price in USD and also are cryptocurrency not real currency.
 func validateData( orginalArray: [SingleCryptoTableViewCellModel]) -> [SingleCryptoTableViewCellModel] {
     var tmpArray = [SingleCryptoTableViewCellModel]()
     for item in orginalArray {
-        if item.price != nil {
             if item.price != 0{
                 tmpArray.append(item)
             }
-        }
     }
     return tmpArray
 }
